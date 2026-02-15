3db13ddd38b04275cbf751c1ebd51adc4bd2e176
rahul31774@gmail.com <rahul31774@gmail.com>
2026-02-15 11:15:34 +0530

    manage relationship with cart and cartItem

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -54,6 +54,11 @@ public class User {
     private Set<Role> roles;
 
     @ToString.Exclude
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST,  CascadeType.MERGE}, orphanRemoval = true)
+    private Cart cart;
+
+
+    @ToString.Exclude
     @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE},
                 orphanRemoval = true)
     private Set<Product> products;
